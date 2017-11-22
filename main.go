d0084351ba9a01af082f37c7d4c514c119d76da1
sherwoodzern <33662771+sherwoodzern@users.noreply.github.com>
2017-11-21 23:08:26 -0500

    Removed the last modification

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World - Happy Thanksgiving Again!!"))
+	w.Write([]byte("Hello World - Happy Thanksgiving!!"))
 }
 
 func main() {
